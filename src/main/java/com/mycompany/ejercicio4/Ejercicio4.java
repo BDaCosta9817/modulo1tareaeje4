b5c06d252e06974ca60b7eb6bc1184fc00806ab0
UserCP <UserCP@DESKTOP-EVBJTP8>
2024-11-03 20:51:33 -0600

    Carga inicial modulo1 ejercicio4

---
+++ src/main/java/com/mycompany/ejercicio4/Ejercicio4.java
@@ -0,0 +1,29 @@
+//Blanca Da Costa Gomez Carcamo
+//202310010031
+//Programa para almacenar nombres de compañeros
+
+
+package com.mycompany.ejercicio4;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        // Crear un arreglo que almacene los nombres
+        String[] nombres = {
+            "Valeria",
+            "Ruth",
+            "Jose",
+            "Carlos",
+            "Andrea",
+            "Hector",
+            "Max",
+            "Luis",
+            "Ludin",
+            "Mayely"
+        };
+
+        System.out.println("Nombres de los compañeros de clase:");
+        for (String nombre : nombres) {
+            System.out.println(nombre);
+        }
+    }
+}
